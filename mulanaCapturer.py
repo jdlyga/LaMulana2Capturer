a70091fc2a591292963bb9dd55ea028a69d300fb
Joe Lyga <jdlyga@gmail.com>
2020-05-08 18:15:52 -0400

    Added clipping regions for tablets, dialog, and messages

--- mulanaCapturer.py
+++ mulanaCapturer.py
@@ -3,19 +3,49 @@ import win32gui
 from google.cloud import vision
 from google.cloud.vision import types
 import io
+import tempfile
+from dataclasses import dataclass
 
 ####################################################################################################
 
 def main():
-
-    imageFile = io.BytesIO()
-    captureWindow('Lamulana2', imageFile)
-    if imageFile:
-        detectText('test.png')
+    captureTablet()
 
 ####################################################################################################
 
-def captureWindow(windowTitle, imageFile):
+@dataclass
+class ClippingRegion:
+    left: int
+    top: int
+    right: int
+    bottom: int
+
+####################################################################################################
+
+def captureTablet():
+    capture(clippingRegion=ClippingRegion(left=200, top=150, right=200, bottom=150))
+
+####################################################################################################
+
+def captureDialog():
+    capture(clippingRegion=ClippingRegion(left=130, top=70, right=770, bottom=380))
+
+####################################################################################################
+
+def captureMessage():
+    capture(clippingRegion=ClippingRegion(left=200, top=170, right=240, bottom=224))
+
+####################################################################################################
+
+def capture(clippingRegion: ClippingRegion):
+
+    tempFileName = tempfile.mktemp()
+    captureWindow(windowTitle='LaMulana2', filename=tempFileName, clippingRegion=clippingRegion)
+    detectText(tempFileName)
+
+####################################################################################################
+
+def captureWindow(windowTitle: str, filename: str, clippingRegion: ClippingRegion):
     
     hwnd = win32gui.FindWindow(None, windowTitle)
 
@@ -27,20 +57,23 @@ def captureWindow(windowTitle, imageFile):
     x, y, x1, y1 = win32gui.GetClientRect(hwnd)
     x, y = win32gui.ClientToScreen(hwnd, (x, y))
     x1, y1 = win32gui.ClientToScreen(hwnd, (x1 - x, y1 - y))
-    PILImage = pyautogui.screenshot(region=(x, y, x1, y1))
-    
-    PILImage.save('test.png')
+    PILImage = pyautogui.screenshot(
+        region=(x + clippingRegion.left, 
+                y + clippingRegion.top, 
+                x1 - clippingRegion.right - clippingRegion.left, 
+                y1 - clippingRegion.top - clippingRegion.bottom))
 
-    return imageFile
+    PILImage.save(filename, format="png")
+    PILImage.show()
 
 ####################################################################################################
 
-def detectText(path):
+def detectText(filename: str):
     """Detects text in the file."""
 
     client = vision.ImageAnnotatorClient()
 
-    with io.open(path, 'rb') as image_file:
+    with io.open(filename, 'rb') as image_file:
         content = image_file.read()
 
     image = vision.types.Image(content=content)
